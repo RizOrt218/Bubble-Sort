336cfdf01bda4ebea55907c26bff2ee117ea0714
RizOrt218 <rizhiafalen@gmail.com>
2015-12-28 16:53:32 -1000

    comments

--- sorting_algo/quickSort.js
+++ sorting_algo/quickSort.js
@@ -1,6 +1,15 @@
 module.exports = quickSort;
 
 function quickSort( numArr ) {
+
+  //grab a number in your array
   var pivot = numArr[0];
   console.log('pivottttting', pivot);
+  var left = [];
+  var right = [];
+
+  //take numbers and compare to your pivot
+  //left => less than pivot
+  //right => greater than pivot
+
 }
