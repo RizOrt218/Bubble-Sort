9c0c839d99ab30cada0e78d98d3a05cdea1a84b1
RizOrt218 <rizhiafalen@gmail.com>
2015-12-28 15:06:53 -1000

    passed own test to sort array of numbers using recursion

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,5 +1,32 @@
 module.exports = bubbleSort;
 
-function bubbleSort() {
+function bubbleSort( numArr ) {
+
+  var count = 0;
+
+  for ( var i = 0; i < numArr.length; i++ ) {
+    var currentNum = numArr[i];
+    var next = numArr[i + 1];
+
+    //switch index if currentNum > next
+    if( currentNum > next ) {
+      numArr[i] = next;
+      numArr[i + 1] = currentNum;
+      count++; //increment count whenever switch takes place
+    }
+    // while( count !== 0 ) {
+    //   return bubbleSort( numArr );
+    // }
+    //return numArr if count
+    if( next === undefined && count === 0 ) {
+      return numArr;
+    }
+    //we've reached the end of array but swapped numbers took place
+    if( next === undefined && count !== 0 ) {
+      return bubbleSort( numArr );
+    }
+  }
 
 }
+// bubbleSort();
+// console.log(bubbleSort());
--- test/bubbleSort.spec.js
+++ test/bubbleSort.spec.js
@@ -8,4 +8,11 @@ describe('bubble sort', function() {
   it('should be a function that exist', function() {
     expect(bubbleSort).to.be.a('function');
   });
+
+  it('should sort an unsorted array', function() {
+    var result = bubbleSort([ 7,5,6,3,4,2,1]);
+
+    expect( result ).to.be.an('array');
+    expect( result ).to.deep.equal([ 1,2,3,4,5,6,7]);
+  });
 });
