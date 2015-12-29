fde41934d4b53674d55c1351350f9a7abf849072
RizOrt218 <rizhiafalen@gmail.com>
2015-12-29 13:10:25 -1000

    creates task to handle sass, livereload

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var sass = require('gulp-sass');
+var connect = require('gulp-connect');
+
+gulp.task('connect', function(){
+  connect.server({
+    root: 'public',
+    livereload: true
+  });
+});
+// keeps gulp from crashing for scss errors
+gulp.task('sass', function () {
+  return gulp.src('./scss/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('livereload', function (){
+  gulp.src('./public/**/*')
+  .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./scss/**/*.scss', ['sass']);
+  gulp.watch('./public/**/*', ['livereload']);
+});
+
+gulp.task('build', function() {
+  gulp.src('./js/app.js')
+        .pipe(browserify({
+          insertGlobals : true
+        }))
+        .pipe(gulp.dest('./js/build'));
+});
+
+gulp.task('default', ['build']);
