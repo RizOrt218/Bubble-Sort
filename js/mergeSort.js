99868bb7e798a01ea14a98cba3906b0669f76492
RizOrt218 <rizhiafalen@gmail.com>
2015-12-29 17:20:07 -1000

    slice numArr from 0 to middle

--- js/mergeSort.js
+++ js/mergeSort.js
@@ -3,15 +3,19 @@ var mergeSortModule = module.exports = (function () {
   return {
 
     mergeSort : function (numArr) {
-      var middle = 0;
+      var middle = Math.floor((numArr.length)/2);
+      var right = [];
 
       if ( numArr.length <= 1 ) {
         return numArr;
       }
+      // for( var i = 0; i < numArr.length; i++ ) {
+        console.log(middle);
+        var left = numArr.slice(0, middle);
+        console.log(left);
 
-      middle = Math.floor((numArr.length)/2);
-      console.log(middle);
-    }
+      }
+    // }
 
 
   };
