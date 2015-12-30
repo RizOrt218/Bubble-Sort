9e7bd9df21910777330c128744e187dee9043fd5
RizOrt218 <rizhiafalen@gmail.com>
2015-12-29 18:02:56 -1000

    creates fn to compare both left and right arrays

--- js/mergeSort.js
+++ js/mergeSort.js
@@ -2,20 +2,27 @@ var mergeSortModule = module.exports = (function () {
 
   return {
 
-    mergeSort : function (numArr) {
+    mergeSort : function( numArr ) {
+      var sortedArray = [];
+      var lt_Index = 0;
+      var rt_Index = 0;
 
       if ( numArr.length <= 1 ) {
         return numArr;
       }
-      var middle = Math.floor((numArr.length)/2); //3
-      var left = numArr.slice(0, middle); //[2, 3, 1]
-      var right = numArr.slice(middle); //[4, 6, 5]
+      var middle = Math.floor( numArr.length/2 ); //3
+      var left = numArr.slice( 0, middle ); //[2, 3, 1]
+      var right = numArr.slice( middle ); //[4, 6, 5]
 
-      if( left[0] < right[0] ){
-        return this.mergeSort( numArr );
+      function compareBoth ( left, right ) {
+        while( leftIndex < left.length && rightIndex < right.length ) {
+          if( left[] < right[0] ){
+
+          }
+        }
       }
 
-      }
+  }
 
 
   };
