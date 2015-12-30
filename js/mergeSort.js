10c79c0bbdfb7944534957f660d91738cbe92796
RizOrt218 <rizhiafalen@gmail.com>
2015-12-29 15:36:40 -1000

    renamed directory file

---
+++ js/mergeSort.js
@@ -0,0 +1,8 @@
+var mergeSortModule = module.exports = (function () {
+
+  return {
+
+
+  };
+}());
+
---
+++ js/quickSort.js
@@ -0,0 +1,24 @@
+module.exports = quickSort;
+
+function quickSort( numArr ) {
+
+  var left = [];
+  var right = [];
+  var pivot = numArr[0];
+//base case
+  if( numArr.length <= 1 ) {
+    return numArr;
+  }
+
+  for( var i = 1; i < numArr.length; i++ ) {
+    if( pivot >= numArr[i] ) {
+      left.push(numArr[i]); //2, 3, 4, 1
+    }
+    else {
+      right.push(numArr[i]); // 7, 6
+    }
+  }
+  //console.log(quickSort(left).concat(pivot, quickSort(right)));
+  return quickSort(left).concat(pivot, quickSort(right));
+
+}
