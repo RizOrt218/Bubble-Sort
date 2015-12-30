2e0e7b754da14abc5914b13104ffb71950cfee94
RizOrt218 <rizhiafalen@gmail.com>
2015-12-29 16:39:12 -1000

    trying to find the middle index

--- js/mergeSort.js
+++ js/mergeSort.js
@@ -2,8 +2,17 @@ var mergeSortModule = module.exports = (function () {
 
   return {
 
-    mergeSort : function (numArry) {
+    mergeSort : function (numArr) {
+      var middle = 0;
 
+      if ( numArr.length <= 1 ) {
+        return numArr;
+      }
+
+      for( var i = 0; i < numArr.length; i++ ) {
+        middle = Math.floor((0 + numArr.length)/2);
+        console.log(middle);
+      }
     }
 
 
